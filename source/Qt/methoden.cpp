cdbd9bc394e14e162a633b4dcf03dc7214c80951
Michel Gisler <Michel Gisler>
2016-05-18 20:39:20 +0200

    Qt Layout & Widget erstellt

---
+++ source/Qt/methoden.cpp
@@ -0,0 +1,7 @@
+
+void QWidget::setFixedSize(int width, int height);
+void QWidget::setGeometry(int x, int y, int w, int h);
+void QWidget::resize(int w, int h);
+void QWidget::move(int x, int y);
+
+
