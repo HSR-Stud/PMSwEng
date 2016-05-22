037e9166343f7f59f8faf94fa3bfbc45706310fa
Michel Gisler <Michel Gisler>
2016-05-22 12:01:40 +0200

    Qt GUI-Programmierung ergänzt, Qt Paint erstellt, Qt Anhang ergänzt TODO QObject, Ergänzungen aus Praktika

---
+++ source/Qt/qpainter.cpp
@@ -0,0 +1,12 @@
+//**** Malvorgang initialisieren
+QPixmap pixmap (160, 160); // Zeichenoberfläche
+QPainter painter (&pixmap); // 'painter' zeichnet auf 'pixmap
+
+//**** Zeichnungsgeräte initialisieren
+QPen pen (Qt::blue, 2); 
+painter.setPen(pen);
+QBrush brush (Qt::green); 
+painter.setBrush(brush)
+
+//**** Zeichenoperation durchführen
+painter.drawEllipse(10, 10, 140, 140); // Kreis zeichnen
