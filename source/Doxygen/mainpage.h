bc602fa792da2aebded3cb546ed1b49bb755ee46
Luca Mazzoleni <lmazzole@hsr.ch>
2016-07-11 14:33:12 +0200

    Fixed the Doxygen-Cmt-Bug

---
+++ source/Doxygen/mainpage.h
@@ -0,0 +1,10 @@
+/**
+ * @mainpage
+ * @brief Hilfsklassen, die häufig gewünschte Fähigkeiten in objektorientierter Form anbieten.
+ *
+ * Momentan werden erst zwei Klassen angeboten
+ *
+ * @see Keyboard Hilft beim Einlesen von der Tastatur.
+ * @see StopWatch Stoppuhr.
+ * @author wb / pl
+ */
